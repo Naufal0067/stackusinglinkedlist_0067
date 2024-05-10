ce80cdd28e4ddcd19daf5714dc2729e7d85e4a93
Naufal0067 <naufalopang1928@gmail.com>
2024-05-10 15:36:30 +0700

    menambahkan cout popped value

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -27,5 +27,7 @@ public:
        if (isEmpty()) {
         cout << "Stack is empty." << endl;
        } 
+        cout << "Popped value: " << top->data << endl;
+        top = top-next; // update the top pointer to the next node
     }
 }
