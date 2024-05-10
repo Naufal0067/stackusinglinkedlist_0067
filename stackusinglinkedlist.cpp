168f0b87d8b637b338b705bd78a5d61e37f60f92
Naufal0067 <naufalopang1928@gmail.com>
2024-05-10 15:37:47 +0700

    memasukkan void peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -29,5 +29,11 @@ public:
        } 
         cout << "Popped value: " << top->data << endl;
         top = top-next; // update the top pointer to the next node
+
+    }
+
+    // peek/top operation: retrieve the value of the topmost elemetn without removing it
+    void peek() {
+        
     }
 }
